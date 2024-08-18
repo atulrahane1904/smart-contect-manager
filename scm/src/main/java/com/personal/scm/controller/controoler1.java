0ba5ad9536d7f073f0a4f116716f36383b989c9d
atul_rahane <atulrahane1904@gmail.com>
2024-08-18 22:29:00 +0530

    working on navbar full responsive

--- scm/src/main/java/com/personal/scm/controller/controoler1.java
+++ scm/src/main/java/com/personal/scm/controller/controoler1.java
@@ -1,17 +1,47 @@
 package com.personal.scm.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 
 @Controller
 public class controoler1 {
  @RequestMapping("/home")
- public String home(Model model){
-     model.addAttribute("name","Atul");
-     model.addAttribute("github", "https://github.com/Rajath1091/Food-App");
+ public String home(){
+    //  model.addAttribute("name","Atul");
+    //  model.addAttribute("github", "https://github.com/Rajath1091/Food-App");
         System.out.println("Home Page controller");
         return "home";
     }
+    @RequestMapping("/contect")
+    public String contect(){
+       //  model.addAttribute("name","Atul");
+       //  model.addAttribute("github", "https://github.com/Rajath1091/Food-App");
+           System.out.println("Contect Page controller");
+           return "contect";
+       }
+    @RequestMapping("/about")
+   public String about(){
+
+   System.out.println("About page loading");
+return "about";
+    }
+    @RequestMapping("/service")
+   public String service(){
+    return "service";
+
+    }
+    @RequestMapping("/login")
+    public String login(){
+    return "login";
+
+    }
+    @RequestMapping("/register")
+    public String register(){
+    return "register";
+
+    }
+    
 }
